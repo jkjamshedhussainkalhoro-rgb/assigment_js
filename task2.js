fadd39a291fc3825fe3a2c8c02426d5124f688c5
jkjamshedhussainkalhoro-rgb <jkjamshedhussainkalhoro@gmail.com>
2026-08-02 13:28:57 +0500

    task two don

---
+++ task2.js
@@ -0,0 +1,14 @@
+let i= -5;
+while (i<=5){
+    if (i>0){
+        console.log(i+= " positive");
+
+    } 
+    else if (i<0){
+     console .log(i +"negative ");
+else if (i<0){
+     console .log(i +"zero ");
+else if (i<0){
+    i++
+    
+}
